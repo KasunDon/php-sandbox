1f89fe2c1507ba5d16bd765f4cf76de99f1ba1ed
KasunDon <KasunDon@users.noreply.github.com>
2015-03-03 12:04:38 +0000

    adding views - dummy mongo collection data

--- app/tests/setup/mongo-setup.js
+++ app/tests/setup/mongo-setup.js
@@ -1,2 +1,3 @@
 use sandbox_dev1;
 db.phpsources.insert({ "_id" : ObjectId("54ad21adf037d0b2047b23c6"), "code" : "&lt;?php\n\necho 'hello world again!';", "output" : "hello world again!", "version" : "5.5.1", "create_time" : "2015-01-07 11:22:25", "expiry" : null, "ip" : "59.152.96.178", "theme" : "xcode", "status" : "active" });
+db.views.insert({ "_id" : ObjectId("54ad21adf037d0b2047b23c6"), "views" : NumberLong(0) });
