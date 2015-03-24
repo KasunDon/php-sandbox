d6bab4a20957fa5bc2c09febece214b434fab901
KasunDon <KasunDon@users.noreply.github.com>
2015-03-24 22:39:29 +0000

    OK system status update

--- app/views/service.blade.php
+++ app/views/service.blade.php
@@ -1,10 +1,14 @@
-<p>Current Service Status : <span class="label label-warning" style="margin-left: 10px;">Stable</span></p>
+<p>Current Service Status : <span class="label label-success" style="margin-left: 10px;">OK</span></p>
 <br>
-<h4>2015-03-24 14:23:06</h4>
+<h4>2015-03-24 22:34:48 GMT </h4>
+<p>Due to earlier database cluster problem, system has lost nearly 50% of data was on our database. 
+    We’ve taken measures to prevent these type of incidents reoccurring again. Apologize for any inconvenience caused.</p>
+<hr>
+<h4>2015-03-24 14:23:06 GMT</h4>
 <p>We've managed stabilize the system, unfortunately system has lost nearly 50% of data within our database cluster. 
     We’ve taken measures to prevent these type of incidents reoccurring again. </p>
 <hr>
-<h4>2015-03-24 13:15:00</h4>
+<h4>2015-03-24 13:15:00 GMT</h4>
 <p>We're experiencing database cluster issues.</p>
 
 
