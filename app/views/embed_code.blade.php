456b1a5c077e4f7e108c26ee9e83c4e370b6487e
Kasun Don <kasun.don@affiliatewindow.com>
2015-09-16 16:00:02 +0100

    adding disqus into phpbox share url pages

--- app/views/embed_code.blade.php
+++ app/views/embed_code.blade.php
@@ -4,3 +4,17 @@
         <textarea id="embed-textarea" style="width: 100%;  height: 40px; resize: none; font-family: sans-serif; font-size: 15px;"><script src="{{ \App\Models\Code::$VIEW_LINK }}embed.js?c={{ $_id }}" type="text/javascript"></script></textarea>
     </div>
  </div>
+<br>
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'phpboxx';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
